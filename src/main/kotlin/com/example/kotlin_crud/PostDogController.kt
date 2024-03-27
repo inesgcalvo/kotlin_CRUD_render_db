65577f97ba2e99825f7436b0f39f6a8e95d8c15f
inesgcalvo <inglez@gmail.com>
2024-03-27 12:28:49 +0100

    json instead of uri

--- src/main/kotlin/com/example/kotlin_crud/PostDogController.kt
+++ src/main/kotlin/com/example/kotlin_crud/PostDogController.kt
@@ -4,31 +4,27 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
+//import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
 import java.util.UUID
 
-
-// http://127.0.0.1:8080/post/dog?name={NAME}&breed={BREED}&birthDate={BIRTHDATE}&mother={MOTHER}&father={FATHER}
+// http://127.0.0.1:8080/dogs?name={NAME}&breed={BREED}&birthDate={BIRTHDATE}&mother={MOTHER}&father={FATHER}
 
 @RestController
 class PostDogController(
     private val jpaDogRepository: JpaDogRepository
 ) {
     @PostMapping("/dogs")
-    fun addDog(@RequestParam("name") name: String,
-               @RequestParam("breed") breed: String,
-               @RequestParam("birthDate") birthDate: String,
-               @RequestParam("mother", required = false) mother: String?,
-               @RequestParam("father", required = false) father: String?): ResponseEntity<JpaDog> {
+    fun addDog(@RequestBody dogRequest: DogRequest): ResponseEntity<JpaDog> {
         try {
             val uuid: String = UUID.randomUUID().toString()
             val dog = JpaDog(
                 id = UUID.fromString(uuid),
-                name = name,
-                breed = breed,
-                birthdate = birthDate,
-                mother = mother,
-                father = father
+                name = dogRequest.name,
+                breed = dogRequest.breed,
+                birthdate = dogRequest.birthDate,
+                mother = dogRequest.mother,
+                father = dogRequest.father
             )
             val savedDog = jpaDogRepository.save(dog)
             return ResponseEntity(savedDog, HttpStatus.CREATED)
@@ -37,5 +33,39 @@ class PostDogController(
         }
     }
 }
+
+data class DogRequest(val name: String,
+                      val breed: String,
+                      val birthDate: String,
+                      val mother: String?,
+                      val father: String?)
+
+//@RestController
+//class PostDogController(
+//    private val jpaDogRepository: JpaDogRepository
+//) {
+//    @PostMapping("/dogs")
+//    fun addDog(@RequestParam("name") name: String,
+//               @RequestParam("breed") breed: String,
+//               @RequestParam("birthDate") birthDate: String,
+//               @RequestParam("mother", required = false) mother: String?,
+//               @RequestParam("father", required = false) father: String?): ResponseEntity<JpaDog> {
+//        try {
+//            val uuid: String = UUID.randomUUID().toString()
+//            val dog = JpaDog(
+//                id = UUID.fromString(uuid),
+//                name = name,
+//                breed = breed,
+//                birthdate = birthDate,
+//                mother = mother,
+//                father = father
+//            )
+//            val savedDog = jpaDogRepository.save(dog)
+//            return ResponseEntity(savedDog, HttpStatus.CREATED)
+//        } catch (e: Exception) {
+//            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+//        }
+//    }
+//}
 
 
--- src/main/kotlin/com/example/kotlin_crud/PutDogController.kt
+++ src/main/kotlin/com/example/kotlin_crud/PutDogController.kt
@@ -1,31 +1,68 @@
 package com.example.kotlin_crud
 
-import org.springframework.web.bind.annotation.*
-import java.util.*
 
-// http://127.0.0.1:8080/put/{NAME}/?newName={NEW_NAME}2&breed={BREED}&birthDate={BIRTHDATE}&mother={MOTHER}&father={FATHER}
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.PathVariable
+//import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import java.util.UUID
+
+// http://127.0.0.1:8080/dogs/{id}/?newName={NAME}2&breed={BREED}&birthDate={BIRTHDATE}&mother={MOTHER}&father={FATHER}
 
 @RestController
-class PutDogController(
+class UpdateDogController(
     private val jpaDogRepository: JpaDogRepository
 ) {
-    @PutMapping("/dogs/{name}")
+    @PutMapping("/dogs/{id}")
     fun updateDog(
-        @PathVariable name: String,
-        @RequestParam("newName") newName: String,
-        @RequestParam("breed") breed: String?,
-        @RequestParam("birthDate") birthDate: String?,
-        @RequestParam("mother", required = false) mother: String?,
-        @RequestParam("father", required = false) father: String?
-    ): JpaDog {
-        val selectedDog = jpaDogRepository.findByName(name)
-        val dog = JpaDog(
-            id = selectedDog!!.id,
-            name = newName,
-            breed = breed,
-            birthdate = birthDate,
-            mother = mother,
-            father = father)
-        return jpaDogRepository.save(dog)
+        @PathVariable("id") id: UUID,
+        @RequestBody dogRequest: DogRequest
+    ): ResponseEntity<JpaDog> {
+        val existingDog = jpaDogRepository.findById(id.toString())
+        return if (existingDog.isPresent) {
+            try {
+                val dog = existingDog.get().apply {
+                    name = dogRequest.name
+                    breed = dogRequest.breed
+                    birthdate = dogRequest.birthDate
+                    mother = dogRequest.mother
+                    father = dogRequest.father
+                }
+                val updatedDog = jpaDogRepository.save(dog)
+                ResponseEntity(updatedDog, HttpStatus.OK)
+            } catch (e: Exception) {
+                ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+            }
+        } else {
+            ResponseEntity(HttpStatus.NOT_FOUND)
         }
     }
+}
+
+//@RestController
+//class PutDogController(
+//    private val jpaDogRepository: JpaDogRepository
+//) {
+//    @PutMapping("/dogs/{name}")
+//    fun updateDog(
+//        @PathVariable name: String,
+//        @RequestParam("newName") newName: String,
+//        @RequestParam("breed") breed: String?,
+//        @RequestParam("birthDate") birthDate: String?,
+//        @RequestParam("mother", required = false) mother: String?,
+//        @RequestParam("father", required = false) father: String?
+//    ): JpaDog {
+//        val selectedDog = jpaDogRepository.findByName(name)
+//        val dog = JpaDog(
+//            id = selectedDog!!.id,
+//            name = newName,
+//            breed = breed,
+//            birthdate = birthDate,
+//            mother = mother,
+//            father = father)
+//        return jpaDogRepository.save(dog)
+//        }
+//    }
